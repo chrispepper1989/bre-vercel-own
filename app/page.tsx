68f3c5957c878b676efcbd6a26166c2bbdc13879
Chris Pepper <chrispepper1989@gmail.com>
2024-09-03 16:31:28 +0100

    Trying even more options

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import {cookies} from "next/headers";
+
 export const dynamic = "force-dynamic"
 export const revalidate= 0;
 import GraphComponent from "@/components/atoms/DecisionComponent";
@@ -5,7 +7,7 @@ import GraphComponent from "@/components/atoms/DecisionComponent";
 
 
 export default function Home() {
-    
+    cookies();
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
